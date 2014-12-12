71b72f727f0ff89b76cd449053daf4375885e2b3
Rik Godschalk <samuraipastacat@gmail.com>
2014-12-12 15:08:50 +0100

    updated README and added Caesar Cipher solution

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ruby-building-blocks
 ====================
+
+First repository for [The Odin Project](http://www.theodinproject.com/ruby-programming/building-blocks).
---
+++ caesar_cipher.rb
@@ -0,0 +1,28 @@
+def caesar_cipher(message, shift)
+	shift = shift.remainder(26) 
+
+	message.split("").map do |letter|
+		number = letter.ord
+
+		if number >= 65 && number <= 90
+			number += shift
+			if number < 65
+				number += 26
+			elsif number > 90
+				number -= 26
+			end
+
+		elsif number >= 97 && number <= 122
+			number += shift
+			if number < 97
+				number += 26
+			elsif number > 122
+				number -= 26
+			end
+		end
+
+		number.chr
+	end.join
+end
+
+puts caesar_cipher("What a string!", 5)
