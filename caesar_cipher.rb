57371a9e63ce724095a47e0f1d815d49dc57543b
Rik Godschalk <samuraipastacat@gmail.com>
2014-12-14 15:16:37 +0100

    Refactored Caesar Cipher solution

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -5,25 +5,26 @@ def caesar_cipher(message, shift)
 		number = letter.ord
 
 		if number >= 65 && number <= 90
-			number += shift
-			if number < 65
-				number += 26
-			elsif number > 90
-				number -= 26
-			end
-
+			number = convert_letter(number, 65, 90, shift)
 		elsif number >= 97 && number <= 122
-			number += shift
-			if number < 97
-				number += 26
-			elsif number > 122
-				number -= 26
-			end
+			number = convert_letter(number, 97, 122, shift)
 		end
 
 		number.chr
 	end
 	.join
+end
+
+def convert_letter(number, low, high, shift)
+	number += shift
+
+	if number < low
+		number += 26
+	elsif number > high
+		number -= 26
+	else
+		number
+	end
 end
 
 puts caesar_cipher("What a string!", 5)
