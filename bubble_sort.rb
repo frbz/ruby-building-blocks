d6935c1272a9e08017353af9cd2f4785475e63f5
Rik Godschalk <samuraipastacat@gmail.com>
2014-12-15 21:45:43 +0100

    Added Bubble Sort By solution

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -10,3 +10,19 @@ def bubble_sort(array)
 end
 
 puts bubble_sort([4,3,78,2,0,2])
+
+def bubble_sort_by(array)
+	array.each_with_index.map do |n, i|
+		(i...array.size).each do |c|
+			arg = yield array[i], array[c]
+			if arg < 0
+				array[i], array[c] = array[c], array[i]
+			end
+		end
+	end
+	puts array
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  right.length - left.length
+end
